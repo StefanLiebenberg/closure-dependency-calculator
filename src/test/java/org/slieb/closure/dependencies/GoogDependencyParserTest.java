e639a7a78a5d8e889d498089950f351a83e5b621
Stefan Liebenberg <siga.fredo@gmail.com>
2015-05-15 21:42:26 +0200

    adding tests

---
+++ src/test/java/org/slieb/closure/dependencies/GoogDependencyParserTest.java
@@ -0,0 +1,67 @@
+package org.slieb.closure.dependencies;
+
+import org.junit.Before;
+import org.junit.Test;
+import slieb.kute.resources.Resources;
+
+import java.io.InputStream;
+
+import static org.junit.Assert.*;
+
+
+public class GoogDependencyParserTest {
+
+    GoogDependencyParser parser;
+
+    @Before
+    public void setup() {
+        parser = new GoogDependencyParser();
+    }
+
+    @Test
+    public void parseBaseFindsNoRequiresOrProvides() throws Throwable {
+        try (InputStream inputStream = getClass().getResourceAsStream("/closure-library/closure/goog/base.js")) {
+            GoogDependencyNode node = parser.parse(Resources.inputStreamResource("base.js", () -> inputStream));
+            assertTrue(node.isBaseFile());
+            assertTrue(node.getRequires().isEmpty());
+            assertTrue(node.getProvides().isEmpty());
+        }
+    }
+
+    @Test
+    public void parseGoogArrayFindsNoRequiresOrProvides() throws Throwable {
+        try (InputStream inputStream = getClass().getResourceAsStream("/closure-library/closure/goog/array/array.js")) {
+            GoogDependencyNode node = parser.parse(Resources.inputStreamResource("base.js", () -> inputStream));
+            assertFalse(node.isBaseFile());
+            assertFalse(node.getRequires().isEmpty());
+            assertEquals(1, node.getRequires().size());
+            assertTrue(node.getRequires().contains("goog.asserts"));
+            assertFalse(node.getProvides().isEmpty());
+            assertEquals(2, node.getProvides().size());
+            assertTrue(node.getProvides().contains("goog.array"));
+            assertTrue(node.getProvides().contains("goog.array.ArrayLike"));
+        }
+    }
+
+
+    @Test
+    public void parseIfConstructs() throws Throwable {
+        String code = "if(COMPILED) { goog.require('x'); } else { goog.require('y'); }";
+        GoogDependencyNode node = parser.parse(Resources.stringResource("/inline.js", code));
+        assertTrue(node.getProvides().isEmpty());
+        assertFalse(node.getRequires().isEmpty());
+        assertEquals(2, node.getRequires().size());
+        assertTrue(node.getRequires().contains("x"));
+        assertTrue(node.getRequires().contains("y"));
+    }
+
+    @Test
+    public void parseDefineTest() throws Throwable {
+        try (InputStream inputStream = getClass().getResourceAsStream("/closure-library/closure/goog/defineclass_test.js")) {
+            assertNotNull(inputStream);
+            GoogDependencyNode node = parser.parse(Resources.inputStreamResource("defineclass_test.js", () -> inputStream));
+            assertTrue(node.getProvides().contains("goog.defineClassTest"));
+            assertTrue(node.getRequires().contains("goog.testing.jsunit"));
+        }
+    }
+}
---
+++ src/test/java/org/slieb/closure/dependencies/GoogResourcesTest.java
@@ -0,0 +1,73 @@
+package org.slieb.closure.dependencies;
+
+import com.google.common.collect.ImmutableList;
+import com.google.javascript.jscomp.SourceFile;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import slieb.kute.api.Resource;
+import slieb.kute.api.ResourceProvider;
+import slieb.kute.resources.Resources;
+
+import java.io.IOException;
+import java.io.Reader;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static slieb.kute.resources.Resources.providerOf;
+
+public class GoogResourcesTest {
+
+    Resource.Readable readableA, readableB, readableC;
+
+    ResourceProvider<Resource.Readable> readables;
+
+    @Before
+    public void setup() {
+        readableA = Resources.stringResource("/path/a", "goog.base = function () {};");
+        readableB = Resources.stringResource("/path/b", "goog.provide('b');");
+        readableC = Resources.stringResource("/path/c", "goog.provide('c');");
+        readables = providerOf(readableA, readableB, readableC);
+    }
+
+    @Test
+    public void testGetCalculator() throws Exception {
+        GoogDependencyCalculator calculator = GoogResources.getCalculator(readables);
+        assertNotNull(calculator);
+        Assert.assertEquals(ImmutableList.of(readableA, readableC),
+                calculator.getResourcesFor(readableC));
+        Assert.assertEquals(ImmutableList.of(readableA, readableB),
+                calculator.getResourcesFor(readableB));
+    }
+
+
+    @Test(expected = RuntimeException.class)
+
+    public void testCalculatorProducesRuntimeError() throws Exception {
+
+        Resource.Readable readable = new Resource.Readable() {
+            @Override
+            public Reader getReader() throws IOException {
+                throw new IOException("fake io");
+            }
+
+            @Override
+            public String getPath() {
+                return "/path";
+            }
+        };
+
+        GoogResources.getCalculator(providerOf(readable)).getDependencyNodes();
+    }
+
+    @Test
+    public void testGetSourceFileFromResource() throws Exception {
+        String content = "var x = y;", path = "/path.js";
+        Resource.Readable resource = Resources.stringResource(path, content);
+        SourceFile sourceFile = GoogResources.getSourceFileFromResource(resource);
+        assertNotNull(sourceFile);
+        assertEquals(content, sourceFile.getCode());
+        assertEquals(path, sourceFile.getOriginalPath());
+
+    }
+}
